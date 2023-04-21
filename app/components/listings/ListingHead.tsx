818c823bcc67152abee485310a801a05c79dfaa1
Ryan Pereira <ryan.dxb92@gmail.com>
2023-04-21 21:48:51 +0530

    Completed Listing Detail Page with Map integration and data from db

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import React, { FC } from "react";
+import Heading from "../Heading";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
 
-const ListingHead = () => {
-  return <div>ListingHead</div>;
+interface ListingHeadProps {
+  title: string;
+  imageSrc: string;
+  locationValue: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead: FC<ListingHeadProps> = ({
+  title,
+  imageSrc,
+  locationValue,
+  id,
+  currentUser,
+}) => {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image
+          src={imageSrc}
+          layout="fill"
+          alt="Image"
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ListingHead;
